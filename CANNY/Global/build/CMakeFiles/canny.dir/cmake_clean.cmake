48d0a969a4ed228fbccd3af27476da3a647cbaee
giolem <giolem>
2016-06-07 19:48:09 -0500

    CANNY

---
+++ CANNY/Global/build/CMakeFiles/canny.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/canny.dir/canny_generated_canny.cu.o"
+  "canny.pdb"
+  "canny"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/canny.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- OpenCV/SOBEL/SobelSecuencial/build/CMakeFiles/sobel.dir/cmake_clean.cmake
+++ OpenCV/SOBEL/SobelSecuencial/build/CMakeFiles/sobel.dir/cmake_clean.cmake
@@ -1,10 +1,10 @@
-FILE(REMOVE_RECURSE
+file(REMOVE_RECURSE
   "CMakeFiles/sobel.dir/sobel.cpp.o"
   "sobel.pdb"
   "sobel"
 )
 
 # Per-language clean rules from dependency scanning.
-FOREACH(lang CXX)
-  INCLUDE(CMakeFiles/sobel.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
+foreach(lang CXX)
+  include(CMakeFiles/sobel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
