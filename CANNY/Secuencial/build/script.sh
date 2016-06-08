6bc84d60b42fa68fb772adc7c2a9f95553c5f684
anaguicas <angelicaaguicas@gmail.com>
2016-06-08 11:23:08 -0500

    script for

--- CANNY/Secuencial/build/script.sh
+++ CANNY/Secuencial/build/script.sh
@@ -4,6 +4,6 @@ do
 	for b in 1 2 3 4 5
 	do
 	echo $i $b >> time2.txt
-	./canny ~/Angie/HPC/CANNY/images/img$i.jpg >> time2.txt
+	./canny ~/Angie/HPC/CANNY/images/img$b.jpg >> time2.txt
 	done
 done
